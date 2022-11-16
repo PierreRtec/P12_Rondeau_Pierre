e8e6ace124e8422b3675a049cb82b61c15ad6d6a
PierreRtec <pierre.rondeau9pro@gmail.com>
2022-11-16 11:52:40 +0100

    ADD registration with back-end and setup admin user group for the management team

--- epiceventsproject/crm_app/views.py
+++ epiceventsproject/crm_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import RegisterSerializer, User
+
+
+class RegisterViewSet(viewsets.ModelViewSet):
+    """
+    Create user account when first connexion, registration view.
+    """
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return User.objects.all()
+
--- epiceventsproject/epiceventsproject/settings.py
+++ epiceventsproject/epiceventsproject/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'epiceventsproject.apps.CrmAppConfig',
-    'epiceventsproject.apps.ManagementAppConfig',
+    'crm_app.apps.CrmAppConfig',
+    'management_app.apps.ManagementAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'epiceventsproject',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
@@ -78,10 +79,14 @@ WSGI_APPLICATION = 'epiceventsproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'admin_dev',
+       'PASSWORD': 'admin',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
