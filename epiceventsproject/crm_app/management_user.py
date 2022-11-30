a22939c2bbb64ef2bc489e18bccd44fb43e94478
PierreRtec <pierre.rondeau9pro@gmail.com>
2022-11-30 18:35:34 +0100

    model management_user.py done

--- epiceventsproject/crm_app/management_user.py
+++ epiceventsproject/crm_app/management_user.py
@@ -3,70 +3,34 @@
 from django.contrib.auth.base_user import BaseUserManager
 
 
-# todo: check ce dont on a besoin pour is_staff et super() gestion admin user type manager (auth\models source)
-class ManagementTeamUser(BaseUserManager):
-    def _create_user(self, username, email, password, **extra_fields):
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password, role, **extra_fields):
         """
-        Create and save a user with the given username, email, and password.
+        Create and save a user with the given username, email, password and role.
         """
-        # if not username:
-        #     raise ValueError("The given username must be set")
-        # email = self.normalize_email(email)
-        # # Lookup the real model class from the global app registry so this
-        # # manager method can be used in migrations. This is fine because
-        # # managers are by definition working on the real model.
-        # GlobalUserModel = apps.get_model(
-        #     self.model._meta.app_label, self.model._meta.object_name
-        # )
-        # username = GlobalUserModel.normalize_username(username)
-        # user = self.model(username=username, email=email, **extra_fields)
-        # user.password = make_password(password)
-        # user.save(using=self._db)
-        # return user
-        pass
 
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        # extra_fields.setdefault("is_staff", False)
-        # extra_fields.setdefault("is_superuser", False)
-        # return self._create_user(username, email, password, **extra_fields)
-        pass
+        extra_fields.setdefault("is_active", True)
 
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
-        # extra_fields.setdefault("is_staff", True)
-        # extra_fields.setdefault("is_superuser", True)
-        #
-        # if extra_fields.get("is_staff") is not True:
-        #     raise ValueError("Superuser must have is_staff=True.")
-        # if extra_fields.get("is_superuser") is not True:
-        #     raise ValueError("Superuser must have is_superuser=True.")
-        #
-        # return self._create_user(username, email, password, **extra_fields)
-        pass
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, role=role, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
-    def with_perm(
-        self, perm, is_active=True, include_superusers=True, backend=None, obj=None
-    ):
-        # if backend is None:
-        #     backends = auth._get_backends(return_tuples=True)
-        #     if len(backends) == 1:
-        #         backend, _ = backends[0]
-        #     else:
-        #         raise ValueError(
-        #             "You have multiple authentication backends configured and "
-        #             "therefore must provide the `backend` argument."
-        #         )
-        # elif not isinstance(backend, str):
-        #     raise TypeError(
-        #         "backend must be a dotted import path string (got %r)." % backend
-        #     )
-        # else:
-        #     backend = auth.load_backend(backend)
-        # if hasattr(backend, "with_perm"):
-        #     return backend.with_perm(
-        #         perm,
-        #         is_active=is_active,
-        #         include_superusers=include_superusers,
-        #         obj=obj,
-        #     )
-        # return self.none()
-        pass
+    def create_superuser(self, username, email, password, **extra_fields):
+        """
+        Create and save a superuser with the given username, email, password, role and 'is_superuser' extra_fields.
+        """
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
+        # add of the role attribute to 1 for management and superuser
+        extra_fields.setdefault("role", 1)
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("SuperUser must have is_staff=True.")
+        if extra_fields.get("role") != 1:
+            raise ValueError("Superuser must have role of management")
+
+        return self.create_user(username, email, password, **extra_fields)
