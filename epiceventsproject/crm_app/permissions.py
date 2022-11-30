5522cd735443d4cd43286d29991b7afcb0daa0a7
PierreRtec <pierre.rondeau9pro@gmail.com>
2022-11-30 18:49:25 +0100

    ADD admin manager permission

--- epiceventsproject/crm_app/permissions.py
+++ epiceventsproject/crm_app/permissions.py
@@ -3,37 +3,12 @@ from rest_framework import permissions
 """CUSTOM PERMISSIONS"""
 
 
-# todo: permissions management_team
-class IsManagementTeamGroup(permissions.BasePermission):
+class IsAdmin(permissions.BasePermission):
     """
-    Check if
-    http_methods :
+    Check if user is admin (manager)
     """
-
     def has_permission(self, request, view):
+        user = request.user
+        if user.is_superuser:
+            return True
 
-        return True
-
-
-# todo: permissions sales_team
-class IsSalesTeamGroup(permissions.BasePermission):
-    """
-    Check if
-    http_methods :
-    """
-
-    def has_permission(self, request, view):
-
-        return True
-
-
-# todo: permissions support_team
-class IsSupportTeamGroup(permissions.BasePermission):
-    """
-    Check if
-    http_methods :
-    """
-
-    def has_permission(self, request, view):
-
-        return True
