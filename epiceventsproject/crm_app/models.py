5cf3f1ea5a42e3a7079a99a3e7286d6e78bd2e42
PierreRtec <pierre.rondeau9pro@gmail.com>
2022-12-06 12:16:48 +0100

    FIX import UserManager from management_user

--- epiceventsproject/crm_app/models.py
+++ epiceventsproject/crm_app/models.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from crm_app.management_user import UserManager
 
 
 class CustomUser(AbstractUser):
